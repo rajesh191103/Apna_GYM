47ad4c487562be1382ce24be2ffca62a8d44c2db
rajesh191103 <155428354+rajesh191103@users.noreply.github.com>
2024-04-25 14:06:40 +0530

    Add files via upload

---
+++ src/utils/fetchData.js
@@ -0,0 +1,24 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '3078e39f8cmsh6df9c1353f0d2f7p1050c7jsn518c8aeff8b2',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key':'3078e39f8cmsh6df9c1353f0d2f7p1050c7jsn518c8aeff8b2',
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+  };
+
+
+export const fetchData = async(url, options) => {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    return data;
+}
+
