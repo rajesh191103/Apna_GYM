8e222d508ae67a59923def7189ff6165312affaf
rajesh191103 <155428354+rajesh191103@users.noreply.github.com>
2024-04-25 14:10:33 +0530

    Add files via upload

---
+++ Apna-Gym-main/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/gym_name.jpg';
+
+const Footer = () => (
+  <Box mt="80px" bgcolor="#0E1111">
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <img src={Logo} alt="logo" style={{ width: '200px', height: '200px' }} />
+    </Stack>
+    <Typography color=" #FBFAF3" variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">&#169; Copyright Team 21IT</Typography>
+  </Box>
+);
+
+export default Footer;
